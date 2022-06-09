154c4417b1427a0080008e9cbef4cad2bfff992f
Jisu <rkfwltn12@naver.com>
2022-06-09 19:29:24 +0900

    hibernate Test 부분 추가
    1. hibernate Test 부분 추가
    	- 지금은 메인에 있지만 추후 test코드에 넣을예정
    2. auth 부분 임시 주석처리
     	- 권한 부분이 정상적으로 동작하지않아 테스트하는데 어려움이 존재
    3. 서비스가 정상적으로 동작하지 않아 starter web과  서버 자동으로 재시작해주는 devtools dependency추가
    	- 실서버에 올릴때에는 devtools를 false로 변경필요

--- src/main/java/name/beptest/api/dto/TestDTO.java
+++ src/main/java/name/beptest/api/dto/TestDTO.java
@@ -5,18 +5,47 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 // https://bamdule.tistory.com/40
 @Entity
+@Table(name = "TEST_TBL")
 public class TestDTO {
 	
 	@Id
+	@Column(name = "seq_no")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer seq;
+	private Integer seqNo;
 	
 	@Column(length = 20, nullable = true)
 	private String text;
 	
+	@Column
 	private Integer num;
+
+
+	public Integer getSeqNo() {
+		return seqNo;
+	}
+
+	public void setSeqNo(Integer seqNo) {
+		this.seqNo = seqNo;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Integer getNum() {
+		return num;
+	}
+
+	public void setNum(Integer num) {
+		this.num = num;
+	}
 }
