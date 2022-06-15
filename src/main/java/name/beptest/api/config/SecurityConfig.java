8d4b3ae5fc11f0178613754069797a55bda5a618
JegalJisu <rkfwltn12@naver.com>
2022-06-15 22:08:02 +0900

    보안관련 config추가

---
+++ src/main/java/name/beptest/api/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package name.beptest.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	// https://bamdule.tistory.com/53
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("").authenticated()
+			.antMatchers("").authenticated()
+			.antMatchers("/**").permitAll();
+		
+		http.formLogin()
+			.loginPage("")
+			.defaultSuccessUrl("/")
+			.permitAll();
+		
+		http.logout()
+			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+			.logoutSuccessUrl("/login")
+			.invalidateHttpSession(true);
+		
+		http.exceptionHandling()
+			.accessDeniedPage("/denied");
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(/* 서비스 */).passwordEncoder(passwordEncoder());
+	}
+}
+
